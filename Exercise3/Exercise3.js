7ceab85d16700cecb7e56abcd3fcddd67a868def
Chimba <chimbeevaa@gmail.com>
2022-10-03 21:26:43 +0800

    031022

--- Exercise3/Exercise3.js
+++ Exercise3/Exercise3.js
@@ -7,4 +7,48 @@
 //Добавить в чат механизм отправки геолокации:
 //При клике на кнопку «Геолокация» необходимо отправить данные серверу и вывести в чат ссылку на https://www.openstreetmap.org/ с вашей геолокацией.
 // Сообщение, которое отправит обратно эхо-сервер, выводить не нужно.
+const wsUri = "wss://echo-ws-service.herokuapp.com";
+const output = document.querySelector(".divOfMessages");
+const btn = document.querySelector('.btn');
+const btnGeo = document.querySelector('.geo');
+let message = document.querySelector(".enterMessage");
+let countMessages = 0;
 
+function writeToScreen(message) {
+    if (countMessages >= 11) { //Очистить окно сообщений
+        while (output.hasChildNodes()) {
+            output.removeChild(output.lastChild);
+        }
+        countMessages = 0;
+    }
+    let pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+    countMessages += 1;
+
+}
+function ready() {
+    websocket.onopen = function (evt) {
+        writeToScreen("CONNECTED");
+    };
+    websocket.onclose = function (evt) {
+        writeToScreen("DISCONNECTED");
+    };
+    websocket.onmessage = function (evt) {
+        writeToScreen(
+            '<span style="color: blue;">RESPONSE: ' + evt.data + '</span>'
+        );
+    };
+    websocket.onerror = function (evt) {
+        writeToScreen(
+            '<span style="color: red;">ERROR:</span> ' + evt.data
+        );
+    };
+}
+let websocket = new WebSocket(wsUri);
+document.addEventListener("DOMContentLoaded", ready);
+btn.addEventListener('click', () => {
+    writeToScreen("SENT: " + message.value);
+    websocket.send(message.value);
+});
